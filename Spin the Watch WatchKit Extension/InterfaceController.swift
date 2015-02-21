145a074d0c9bda4f3bc56df991566eae27d077f2
Robert Vargas <vargasvisuals@gmail.com>
2015-02-21 17:53:19 -0500

    initial commit

---
+++ Spin the Watch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Spin the Watch WatchKit Extension
+//
+//  Created by Rob Vargas on 2/21/15.
+//  Copyright (c) 2015 Robert Vargas. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
