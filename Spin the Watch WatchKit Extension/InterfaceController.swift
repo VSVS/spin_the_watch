a81d95e143147525c1629db611f22669b69f6b76
Robert Vargas <vargasvisuals@gmail.com>
2015-03-01 23:47:23 -0500

    code cleanup, layout change

--- Spin the Watch WatchKit Extension/InterfaceController.swift
+++ Spin the Watch WatchKit Extension/InterfaceController.swift
@@ -6,26 +6,44 @@
 //  Copyright (c) 2015 Robert Vargas. All rights reserved.
 //
 
+
 import WatchKit
 import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    
+    @IBOutlet var nameLabel: WKInterfaceLabel!
+    
+    
+    @IBAction func buttonPressed() {
+        
+        
+        
+        nameLabel.setText("My app is working!")
+        
+    }
+    
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
+        println("My app is working!")
+        
+        
+        
     }
-
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        NSLog("%@ will activate", self)
     }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
         super.didDeactivate()
     }
-
+    
 }
